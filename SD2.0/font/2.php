7765ad5febf26e30d3d484f1d43e0e239bb49c32
cgirl <1579090779@qq.com>
2015-09-15 16:42:53 +0800

    系统基本页面开发完成

---
+++ SD2.0/font/2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>登录页面</title>
+
+<link href="css/jquery.mobile-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/jquery.mobile.external-png-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/jquery.mobile.icons-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/jquery.mobile.inline-png-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/jquery.mobile.inline-svg-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/jquery.mobile.structure-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/jquery.mobile.theme-1.4.5.min.css" rel="stylesheet" type="text/css"/>
+<link href="bootstrap-3.3.5-dist/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+<link href="bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="css/my.css" rel="stylesheet" type="text/css"/>
+
+<script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/jquery.mobile-1.4.5.min.js" type="text/javascript"></script>
+<script src="bootstrap-3.3.5-dist/js/bootstrap.js" type="text/javascript"></script>
+<script src="bootstrap-3.3.5-dist/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="bootstrap-3.3.5-dist/js/npm.js" type="text/javascript"></script>
+
+</head> 
+<body>
+	<div data-role="page">
+    	<header>
+    		<h1>商户自助服务平台</h1>
+    	</header>
+        <section data-role="content" id="mmmm">
+        	
+        </section>
+        <footer data-role="header">
+        	
+        </footer>
+    </div>
+</body>
+</html>
